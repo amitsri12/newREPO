001f51cac6dd46f6dbc7187a2f17def6e8435ce4
Amit <amitsrivastav983@gmail.com>
2022-04-14 11:19:07 +0530

    ALL commit

--- D.java
+++ D.java
@@ -1,5 +1,15 @@
 class D {
+public static void main (String arg[]){
+String s="Ram kumar";
+String s1="";
+for (int i=0;i<s.length();i++){
+s1=s.charAt(i)+s1;
 
+
+}
+System.out.println(s1);
+
+}
 
 
 }
